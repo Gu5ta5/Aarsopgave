1424f80b2c7c4b9a769dbe3fdf239b5a3ae61125
Gu5ta5 <gustaspranskus@gmail.com>
2025-03-07 11:04:58 +0100

    Jeg har løst opgave A

--- prog-2g-main/prog-2g-main/aarsopgave/shopping_list.py
+++ prog-2g-main/prog-2g-main/aarsopgave/shopping_list.py
@@ -1,37 +1,22 @@
 class ShoppingList:
-    """
-    A class to represent a shopping list.
-
-    Attributes
-    ----------
-    items : list
-        A list to store shopping items.
-
-    Methods
-    -------
-    add_item(item):
-        Adds an item to the shopping list.
-    
-    remove_item(item):
-        Removes an item from the shopping list if it exists.
-    
-    get_items():
-        Returns the list of shopping items.
-    """
+   
     def __init__(self):
-        self.items = []
+        self.items = {}
 
-    def add_item(self, item):
-        self.items.append(item)
+    def add_item(self, item, quantity=1):
+        if item in self.items:
+            self.items[item] += quantity
+        else:
+            self.items[item] = quantity
 
     def remove_item(self, item):
         if item in self.items:
-            self.items.remove(item)
+            del self.items[item]
 
     def get_items(self):
         return self.items
 
-# Example usage:
+# Eksempel på brug:
 if __name__ == "__main__":
     shopping_list = ShoppingList()
     shopping_list.add_item("Apples")
